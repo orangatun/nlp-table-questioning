a0f5e39e8ed69746ec447f7c3c9cfa88102a4fdc
orangatun <raghunadhan@protonmail.com>
2024-04-04 22:52:37 -0700

    Adds file upload
    Adds file upload
    Validates that the file is .csv

--- home.py
+++ home.py
@@ -1,14 +1,43 @@
+import os
 import functools
 
 from flask import (
-    Blueprint, flash, g, redirect, render_template, request, session, url_for
+    Blueprint, flash, g, redirect, render_template, request, session, url_for, current_app
 )
+from werkzeug.utils import secure_filename
 
 bp = Blueprint('home', __name__, url_prefix='/')
 
-@bp.route('/')
+UPLOAD_FOLDER = os.path.abspath('../uploads')
+ALLOWED_EXTENSIONS = {'csv'}
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@bp.route('/', methods = ('GET', 'POST'))
 def home():
     if session:
+        if request.method == 'POST':
+            # check if the post request has the file part
+            if 'file' not in request.files:
+                flash('No file part')
+                return redirect(request.url)
+            file = request.files['file']
+            # If the user does not select a file, the browser submits an
+            # empty file without a filename.
+            if file.filename == '':
+                flash('No selected file')
+                return redirect(request.url)
+            if file and allowed_file(file.filename):
+                filename = secure_filename(file.filename)
+                file.save(os.path.join(current_app.instance_path+"/uploads", filename))
+                session['file'] = filename
+                flash("File uploaded successfully")
+            else:
+                flash('Invalid file extension')
+                # return redirect(url_for('download_file', name=filename))
         return render_template('home/user-home.html')
     else:
         return render_template('home/general-home.html')
